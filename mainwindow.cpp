d16cbd181ce5e0cbd984ce717b1f484de3cf44b8
sunxin <darksun190@gmail.com>
2013-08-22 17:47:32 +0200

    make points transfer from base alginment to the feature alignment use actual lead for axial fft calculate.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,6 +18,8 @@
 #include <cmath>
 #include <qmath.h>
 #include <math.h>
+#include <matrix.h>
+#include <matrixmath.h>
 
 using namespace splab;
 using namespace std;
@@ -42,6 +44,76 @@ MainWindow::MainWindow(QWidget *parent) :
     if (geo_type == QString("Circle") or geo_type == QString("Cylinder"))
     {
         int size = xml_info.names.at(0).points.size();
+        QVector<point> point_trans; //save the data after alignment transform
+        double radius = xml_info.names.at(0).radius;
+        double lead;         //actual lead for the cylinder
+        QVector<double> angle_commu;
+        {
+            /* transform the coordinate of the data.
+             * special program offer the zero position of the circle or cylinder
+             * use it as new 0 zero point,
+             * O = (x0,y0,z0)
+             * special program offer the vector of the cylinder's axis, use it as Z axis
+             * the formula is
+             * if the vector is (l,m,n)
+             * the matrix Mz is
+             *                  Keep the X axis in XOY plane
+             * |    - m / sqrt ( 1-n^2 )    ,   l / sqrt ( 1-n^2 )      ,   0               |
+             * |    - n*l / sqrt ( 1-n^2 )  ,   - m*n / sqrt ( 1-n^2 )  ,   sqrt ( 1-n^2 )  |
+             * |            l               ,           m               ,       n           |
+             *
+             *                  Keep the X axis in XOZ plane
+             * |     n / sqrt ( 1-m^2 )     ,           0           ,  - l / sqrt( 1-m^2 )  |
+             * |    - n*l / sqrt ( 1-m^2 )  ,   sqrt ( 1-m^2 )      ,  - m*n / sqrt ( 1-m^2)|
+             * |            l               ,           m           ,           n           |
+             *
+             *
+             * the new points' coordinate P' = Mz * (P-O)
+             */
+            //get the new Zero point
+            Vector <double> zero_point(3);
+            zero_point(1) = xml_info.names.at(0).p.x;
+            zero_point(2) = xml_info.names.at(0).p.y;
+            zero_point(3) = xml_info.names.at(0).p.z;
+            qDebug()<<zero_point(1)<<zero_point(2)<<zero_point(3);
+            Matrix <double> Mz;
+            {
+                //calculate the matrix Mz
+                double l,m,n;
+                l = xml_info.names.at(0).v.u;
+                m = xml_info.names.at(0).v.v;
+                n = xml_info.names.at(0).v.w;
+                Mz.resize(3,3);
+                Mz(1,1) = n / sqrt ( 1-m*m );
+                Mz(1,2) = 0;
+                Mz(1,3) = -l / sqrt ( 1-m*m );
+                Mz(2,1) = - n*l / sqrt ( 1-m*m );
+                Mz(2,2) = sqrt ( 1-m*m );
+                Mz(2,3) = - m*n / sqrt ( 1-m*m );
+                Mz(3,1) = l;
+                Mz(3,2) = m;
+                Mz(3,3) = n;
+            }
+            //start transform
+            {
+                point tmp;
+                foreach (tmp, xml_info.names.at(0).points)
+                {
+                    Vector<double> o(3);
+                    o(1) = tmp.x;
+                    o(2) = tmp.y;
+                    o(3) = tmp.z;
+                    Vector<double> t;
+                    t = Mz * (o-zero_point);
+                    point buf_point;
+                    buf_point.x = t(1);
+                    buf_point.y = t(2);
+                    buf_point.z = t(3);
+                    point_trans.push_back(buf_point);
+                    //qDebug()<<o(1)<<o(2)<<o(3)<<"\t"<<t(1)<<t(2)<<t(3);
+                }
+            }
+        }
         //check the direction
         if (size<9)
         {
@@ -49,9 +121,9 @@ MainWindow::MainWindow(QWidget *parent) :
         }
         int direction;  //  -1 = clockwise  ;   1 = anticlockwise
         double angle1,angle3,angle5;
-        angle1 = std::atan2(xml_info.names.at(0).points.at(0).y,xml_info.names.at(0).points.at(0).x);
-        angle3 = std::atan2(xml_info.names.at(0).points.at(4).y,xml_info.names.at(0).points.at(4).x);
-        angle5 = std::atan2(xml_info.names.at(0).points.at(8).y,xml_info.names.at(0).points.at(8).x);
+        angle1 = std::atan2(point_trans.at(0).y,point_trans.at(0).x);
+        angle3 = std::atan2(point_trans.at(4).y,point_trans.at(4).x);
+        angle5 = std::atan2(point_trans.at(8).y,point_trans.at(8).x);
         if ((angle5-angle3)*(angle3-angle1)<0)
         {
             //means the start point nearby the 180-degree
@@ -75,11 +147,12 @@ MainWindow::MainWindow(QWidget *parent) :
 
         //caculate the angle of the element
         double round_nr=0;
+        angle_commu.push_back(round_nr);
         double next_angle,last_angle;
         last_angle = angle1;
         for (int i=1;i<size;++i)
         {
-            next_angle = std::atan2(xml_info.names.at(0).points.at(i).y,xml_info.names.at(0).points.at(i).x);
+            next_angle = std::atan2(point_trans.at(i).y,point_trans.at(i).x);
             if (std::fabs(next_angle-last_angle)>PI)
             {
                 if (direction == 1)
@@ -97,142 +170,83 @@ MainWindow::MainWindow(QWidget *parent) :
             }
             //qDebug()<<last_angle<<"\t"<<next_angle<<"\t"<<next_angle-last_angle<<"\t"<<round_nr;
             last_angle = next_angle;
+            angle_commu.push_back(round_nr);
 
         }
-        double radius = xml_info.names.at(0).radius;
+
+        //calculate the lead
+        lead = (point_trans.last().z-point_trans.first().z) / (round_nr/2/PI);
         {
-            //calculating the FFT
+            //calculating the axial FFT
+            Vector< complex<double> >Rk;
+            Vector<double> rn;
+            // get the origin data from special program
+            rn.resize(size);
+            for(int i=0;i<size;++i)
+            {
+                rn[i] = point_trans.at(i).z - lead * (angle_commu.at(i)/2/PI);
+                //rn[i] = hypot(point_trans.at(i).y,point_trans.at(i).x)-radius;
+            }
+            Rk = fftr2c( rn );
+
+            for (int j=0;j<300;j++)
+            {
+                fft_axial.push_back(sqrt(pow(Rk[j].real(),2)+pow(Rk[j].imag(),2))/size);
+            }
+
+            double a;
+            list2.clear();
+            foreach (a,fft_axial)
+            {
+                list2.push_back(QString::number(a*1000));
+            }
+
+            model2 = new QStringListModel();
+            model2->setStringList(list2);
+            ui->tableView_2->setModel(model2);
+            QVector<double>::Iterator ib=fft_axial.begin();
+            ib++;
+
+            scale_fft_axial_y = 250 / (*max_element(ib,fft_axial.end()));
+        }
+        {
+            //calculating the radial FFT
             Vector< complex<double> >  Rk;
             Vector<double> rn;
             // get the origin data from special program
             rn.resize(size);
             for(int i=0;i<size;++i)
             {
-                rn[i] = hypot(xml_info.names.at(0).points.at(i).y,xml_info.names.at(0).points.at(i).x)-radius;
+                rn[i] = hypot(point_trans.at(i).y,point_trans.at(i).x)-radius;
 
             }
             Rk = fftr2c( rn );
 
-            for (int j=0;j<200;j++)
+            for (int j=0;j<300;j++)
             {
-                zmfft.push_back(sqrt(pow(Rk[j].real(),2)+pow(Rk[j].imag(),2))/size);
+                fft_radial.push_back(sqrt(pow(Rk[j].real(),2)+pow(Rk[j].imag(),2))/size);
             }
 
             double a;
-            QStringList list2;
-
-            foreach (a,zmfft)
+            list1.clear();
+            foreach (a,fft_radial)
             {
-                list2.push_back(QString::number(a));
+                list1.push_back(QString::number(a*1000));
             }
 
-            model2 = new QStringListModel();
-            model2->setStringList(list2);
-            ui->tableView_2->setModel(model2);
+            model1 = new QStringListModel();
+            model1->setStringList(list1);
+            ui->tableView->setModel(model1);
+            QVector<double>::Iterator ib=fft_radial.begin();
+            ib++;
+            scale_fft_radial_y = 250 / (*max_element(ib,fft_radial.end()));
             //qDebug()<<"point number is: " << size;
         }
-        qDebug()<<angle1<<"\t"<<round_nr/2/PI;
+        qDebug()<<"The real Lead is "<<"\t"<<lead;
+        qDebug()<<scale_fft_radial_y;
     }
 
-    /*
-     // here is the code for the old lib;
-    QString fileName = xml_info.names.at(0).Identifier;
 
-    QFile nom_file(QString("../tmp/%1_NomPoints.txt").arg(fileName));
-    QFile act_file(QString("../tmp/%1_Points.txt").arg(fileName));
-
-
-    nom_file.open(QFile::ReadOnly|QFile::Text);
-    act_file.open(QFile::ReadOnly|QFile::Text);
-
-    QTextStream in_nom(&nom_file);
-    QTextStream in_act(&act_file);
-
-    QTextStream out(stdout);
-
-    QVector <double> real_n;
-    int size;
-    while(1)
-    {
-        QString buf2;
-        buf2 = in_act.readLine();
-        if(buf2.isNull())
-            break;
-        QStringList temp = buf2.split(" ");
-        double tx,ty;
-        tx = temp[0].toDouble();
-        ty = temp[1].toDouble();
-        real_n.push_back(sqrt(tx*tx+ty*ty));
-    }
-     size = real_n.size();
-    int channel;
-    double re,im;
-
-    double divi = double(size) / (2 * 3.14159265359);
-    QTime t;
-    t.start();
-    for (channel = 0;channel<127;channel++)
-    {
-        for(int i = 0;i<size;++i)
-        {
-            re +=  (sin((i / divi) * channel) * real_n[i]) / size; // 'realanteil integration
-            im +=  (cos((i / divi) * channel) * real_n[i]) / size; // 'imaginäranteil
-        }
-        yfft.push_back(sqrt((re * re) + (im * im))); //            'Amplitude berechen
-        re = 0;
-        im = 0;
-        //qDebug()<<channel<<"\t"<<yfft[channel]*2000;
-    }
-    //qDebug("Time elapsed: %d ms", t.elapsed());
-    {
-     Vector< complex<Type> >  sn,Sk,Rk;
-     Vector<Type> rn, tn;
-     // get the origin data from special program
-     sn.resize(size);
-     rn.resize(size);
-     int i=0;
-     for(i=0;i<size;++i)
-     {
-         rn[i] = real_n[i];
-      }
-    t.restart();
-     Rk = fftr2c( rn );
-      //qDebug("Time elapsed: %d ms", t.elapsed());
-
-      for (int j=0;j<i;j++)
-      {
-          //qDebug()<<Rk[j].real()<<"\t"<<Rk[j].imag()<<"----\t----"<<sqrt(pow(Rk[j].real(),2)+pow(Rk[j].imag(),2));
-
-          //qDebug()<<sn[i].real()<<"\t"<<sn[i].imag();
-          //qDebug()<<Sk[j].real()<<"\t"<<Sk[j].imag();
-          zmfft.push_back(sqrt(pow(Rk[j].real(),2)+pow(Rk[j].imag(),2))/size);
-
-      }
-
-    }
-    double a;
-    QStringList list,list2;
-    foreach (a,yfft)
-    {
-        list.push_back(QString::number(a*2000));
-    }
-    foreach (a,zmfft)
-    {
-        list2.push_back(QString::number(a*3));
-    }
-    model = new QStringListModel();
-    model->setStringList(list);
-    ui->tableView->setModel(model);
-    model2 = new QStringListModel();
-    model2->setStringList(list2);
-    ui->tableView_2->setModel(model2);
-    //qDebug()<<"point number is: " << size;
-    for(int i=0;i<127;++i)
-    {
-        ;
-        //qDebug()<<QString("%1\t%2\t%3\t%4").arg(i).arg(zmfft[i]).arg(yfft[i]).arg(zmfft[i]/yfft[i]);
-    }
-    */
 }
 
 MainWindow::~MainWindow()
@@ -246,27 +260,31 @@ void MainWindow::paintEvent(QPaintEvent *)
     painter.save();
     painter.setPen(Qt::SolidLine);
 
-    painter.translate(100,200);
-/*    //painter.scale(3,2);
+    //painter for radial
+    // the drawing area is 576*250
+    painter.translate(100,300);
+       //painter.scale(3,2);
     painter.drawLine(0,0,400,0);
     painter.drawLine(0,0,100,0);
     painter.drawText(0,10,QString("0"));
     painter.drawText(384,10,QString("128"));
 
-    for(int i=1;i<128;++i)
+    for(int i=1;i<300;++i)
     {
-        painter.drawLine(i*3,0,i*3,-yfft[i]*100000);
+        painter.drawLine(i,0,i,-fft_radial[i]*scale_fft_radial_y);
     }
-*/
+
+    //painter for axial
+    // the drawing area is 576*250
     painter.translate(0,300);
     painter.drawLine(0,0,400,0);
     painter.drawLine(0,0,100,0);
     painter.drawText(0,10,QString("0"));
     painter.drawText(384,10,QString("128"));
 
-    for(int i=1;i<128;++i)
+    for(int i=1;i<300;++i)
     {
-        painter.drawLine(i*3,0,i*3,-zmfft[i]*10000);
+        painter.drawLine(i,0,i,-fft_axial[i]*scale_fft_axial_y);
     }
 
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,10 +15,16 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-        QVector<double> yfft;
-        QVector<double> zmfft;
-        QStringListModel *model;
+        QVector<double> fft_radial;
+        QVector<double> fft_axial;
+        QStringListModel *model1;
         QStringListModel *model2;
+
+        QStringList list1;      // save the radial fft result for listmodel
+        QStringList list2;      // save the axial fft result for listmodel
+
+        double scale_fft_axial_y;
+        double scale_fft_radial_y;
 private:
     Ui::MainWindow *ui;
     void paintEvent(QPaintEvent *);
